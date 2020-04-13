d882cbcf5bfdbf8f4e50f39948a344d66940ac7d
Jingren Wang <jingren.wang@minerva.kgi.edu>
2020-04-13 19:52:57 +0800

    changed expression
    changed expression

--- tests/integrate.py
+++ tests/integrate.py
@@ -7,7 +7,7 @@ class IntegrateTests(unittest.TestCase):
 
 
     def test_post(self):
-        r = requests.post('http://localhost:5000/'+ “add”, data = {‘expression’: ‘8+7+6+5+4+3+2+1’})
+        r = requests.post('http://localhost:5000/'+ “add”, data = {‘expression’: ‘3240532+3413+1000’})
         self.assertEqual(r.status_code, 200)
 
 
