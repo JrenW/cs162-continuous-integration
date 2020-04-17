4120a31d483257fcb8b72b94c7e26b4e29943d1f
Jingren Wang <jingren.wang@minerva.kgi.edu>
2020-04-17 17:41:30 +0800

    added third test
    added third test to test invalid user input in test/integrate.py

--- tests/integrate.py
+++ tests/integrate.py
@@ -4,7 +4,7 @@ import unittest
 
 URL = "http://localhost:5000/"
 
-class TestWeb(unittest.TestCase):
+class IntegrationTests(unittest.TestCase):
 
     def test_post(self):
         r = requests.post(URL + "add", data = {'expression': '333+22+11+5'})
@@ -14,6 +14,11 @@ class TestWeb(unittest.TestCase):
         r = requests.post(URL + "add", data = {'expression': '32-6+22/5'})
         self.assertEqual(r.status_code, 200)
       
+    def test_post(self):
+        '''invalid user input'''
+        r = requests.post(URL + "add", data = {'expression': '32$6-'})
+        # error 503 should be caught: the server is unable to handle the request
+        self.assertEqual(r.status_code, 503)
       
 if __name__ == '__main__':
     unittest.main()
