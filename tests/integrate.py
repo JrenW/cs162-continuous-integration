f965ba2225ca5ba736ec9c91acf03b9a03a8aed6
Jingren Wang <jingren.wang@minerva.kgi.edu>
2020-04-17 17:18:17 +0800

    update yml.file
    changed dependency file before test script run

--- tests/integrate.py
+++ tests/integrate.py
@@ -1,16 +1,13 @@
-'''integration test cases using unittest module'''
+'''a simple integration test to send a request to the app server'''
 
-
+import request
 import unittest
 
 class IntegrateTests(unittest.TestCase):
 
-
     def test_post(self):
         r = requests.post('http://localhost:5000/'+ “add”, data = {‘expression’: ‘3240532+3413+1000’})
         self.assertEqual(r.status_code, 200)
-
-
 
 if __name__ == '__main__':
     unittest.main()
